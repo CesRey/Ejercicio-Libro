31cf5e67a1ace96b96666f41cb94eeb054cf3d52
Cèsar <pacblack@outlook.com>
2017-08-24 10:34:34 -0500

    Se Realizo actualización del software

---
+++ Libro.java
@@ -0,0 +1,11 @@
+
+public class Libro
+{
+    private String Titulo;
+    private String Autor;
+    public Libro(String Titule, String Autore)
+    {
+        Titulo=Titule;
+        Autor=Autore;
+    }
+}

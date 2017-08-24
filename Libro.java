61793506c8e490e905f284b3e146b2c720cf846c
Cèsar <pacblack@outlook.com>
2017-08-24 11:00:28 -0500

    Se agrego el metodo de paginas y de detalles

--- Libro.java
+++ Libro.java
@@ -3,17 +3,35 @@ public class Libro
 {
     private String Titulo;
     private String Autor;
+    private int Paginas;
+    
     public Libro(String Titule, String Autore)
     {
         Titulo=Titule;
         Autor=Autore;
+        Paginas=5;
     }
+    
+    public int dimePaginas(int pag)
+    {
+        Paginas=pag;
+        return pag;
+    }
+    
     public String dimeAutor()
     {
        return Autor;
     }
+    
     public String dimeTitulo()
     {
        return Titulo;
+    }
+    
+    public String dimedetalles()
+    {
+        String detalles;
+        detalles= "Titulo"+Titulo+" "+"Autor"+Autor+"Paginas"+Paginas;
+        return detalles;
     }
 }
